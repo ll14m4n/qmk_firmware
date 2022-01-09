b23c8eceb4e8ee5fec171ea472f43bd5d213bacc
ll14m4n <ll14m4n@gmail.com>
2022-01-09 23:54:22 +0300

    fix

--- keyboards/lily58/keymaps/ll14m4n/keymap.c
+++ keyboards/lily58/keymaps/ll14m4n/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_ISRT] = LAYOUT(
   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_GRV,
-  KC_TAB,  KC_W,    KC_C,    KC_L,    KC_M,    KC_K,                     KC_Z,    KC_F,    KC_U,    KC_COMM, KC_QUOTE, KC_MINS,
+  KC_TAB,  KC_Y,    KC_C,    KC_L,    KC_M,    KC_K,                     KC_Z,    KC_F,    KC_U,    KC_COMM, KC_QUOTE, KC_MINS,
   KC_LCTRL,KC_I,    KC_S,    KC_R,    KC_T,    KC_G,                     KC_P,    KC_N,    KC_E,    KC_A,    KC_O,     KC_SCLN,
   KC_LSFT, KC_Q,    KC_V,    KC_W,    KC_D,    KC_J,    KC_QWER,KC_F13, KC_B,    KC_H,    KC_SLSH, KC_DOT,  KC_X,     KC_RSFT,
                              KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_ENT,  RAISE,   KC_BSPC, KC_RGUI
