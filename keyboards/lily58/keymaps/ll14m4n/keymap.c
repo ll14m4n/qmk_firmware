1cf124eb6df9caf15ba7747549338326d15abb01
ll14m4n <ll14m4n@gmail.com>
2022-01-10 06:59:10 +0300

    .

--- keyboards/lily58/keymaps/ll14m4n/keymap.c
+++ keyboards/lily58/keymaps/ll14m4n/keymap.c
@@ -42,19 +42,19 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWER] = LAYOUT(
-  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_ISRT,KC_F13, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                             KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_ENT,  RAISE,   KC_BSPC, KC_RGUI
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_ISRT, KC_F13,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+          MT(MOD_MEH,KC_NO), KC_LALT, KC_LGUI, LT(2, KC_SPACE), KC_ENT,  RAISE,   KC_BSPC, KC_DEL
 ),
 
  [_ISRT] = LAYOUT(
-  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_GRV,
-  KC_TAB,  KC_Y,    KC_C,    KC_L,    KC_M,    KC_K,                     KC_Z,    KC_F,    KC_U,    KC_COMM, KC_QUOTE, KC_MINS,
-  KC_LCTRL,KC_I,    KC_S,    KC_R,    KC_T,    KC_G,                     KC_P,    KC_N,    KC_E,    KC_A,    KC_O,     KC_SCLN,
-  KC_LSFT, KC_Q,    KC_V,    KC_W,    KC_D,    KC_J,    KC_QWER,KC_F13, KC_B,    KC_H,    KC_SLSH, KC_DOT,  KC_X,     KC_RSFT,
-                             KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_ENT,  RAISE,   KC_BSPC, KC_RGUI
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, KC_Y,    KC_C,    KC_L,    KC_M,    KC_K,                      KC_Z,    KC_F,    KC_U,    KC_COMM, KC_QUOTE, KC_MINS,
+  _______, KC_I,    KC_S,    KC_R,    KC_T,    KC_G,                      KC_P,    KC_N,    KC_E,    KC_A,    KC_O,     KC_SCLN,
+  _______, KC_Q,    KC_V,    KC_W,    KC_D,    KC_J,    KC_QWER, KC_F13,  KC_B,    KC_H,    KC_SLSH, KC_DOT,  KC_X,     _______,
+                             _______, _______, _______, _______, _______, _______,  _______, _______
 ),
 
 [_LOWER] = LAYOUT(
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, XXXXXXX,
   _______, G(KC_Z), G(KC_X), G(KC_C), G(KC_V), XXXXXXX, KC_F15,  XXXXXXX, XXXXXXX, PREV_TB, NEXT_TB, XXXXXXX, XXXXXXX, _______,
-                             _______, _______, _______, _______, _______, _______, KC_DEL,  _______
+                             _______, _______, _______, _______, _______, _______, _______,  _______
 ),
 
   [_ADJUST] = LAYOUT(
@@ -185,7 +185,7 @@ bool oled_task_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case KC_ISRT:
-            if (record->event.pressed) {
+            if (record->event.pressed && !get_mods()){
                 default_layer_set(1UL << _ISRT);
                 tap_code(KC_F13);
                 #ifdef OLED_ENABLE
@@ -197,7 +197,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
 
         case KC_QWER:
-            if (record->event.pressed) {
+            if (record->event.pressed && !get_mods()) {
                 default_layer_set(1UL << _QWER);
                 tap_code(KC_F13);
                 #ifdef OLED_ENABLE
